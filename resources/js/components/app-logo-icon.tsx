fab3d763bccc177b0dc4ca4f37da614541553a77
rflihmmm <indomilk04@gmail.com>
2025-04-10 19:03:57 +0800

    replace default icon with Jubir.ai icon

--- resources/js/components/app-logo-icon.tsx
+++ resources/js/components/app-logo-icon.tsx
@@ -2,12 +2,40 @@ import { SVGAttributes } from 'react';
 
 export default function AppLogoIcon(props: SVGAttributes<SVGElement>) {
     return (
-        <svg {...props} viewBox="0 0 40 42" xmlns="http://www.w3.org/2000/svg">
+        <svg width="500" height="500" viewBox="0 0 500 500" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <rect width="100%" height="100%" class="svg-background" fill="white" />
             <path
-                fillRule="evenodd"
-                clipRule="evenodd"
-                d="M17.2 5.63325L8.6 0.855469L0 5.63325V32.1434L16.2 41.1434L32.4 32.1434V23.699L40 19.4767V9.85547L31.4 5.07769L22.8 9.85547V18.2999L17.2 21.411V5.63325ZM38 18.2999L32.4 21.411V15.2545L38 12.1434V18.2999ZM36.9409 10.4439L31.4 13.5221L25.8591 10.4439L31.4 7.36561L36.9409 10.4439ZM24.8 18.2999V12.1434L30.4 15.2545V21.411L24.8 18.2999ZM23.8 20.0323L29.3409 23.1105L16.2 30.411L10.6591 27.3328L23.8 20.0323ZM7.6 27.9212L15.2 32.1434V38.2999L2 30.9666V7.92116L7.6 11.0323V27.9212ZM8.6 9.29991L3.05913 6.22165L8.6 3.14339L14.1409 6.22165L8.6 9.29991ZM30.4 24.8101L17.2 32.1434V38.2999L30.4 30.9666V24.8101ZM9.6 11.0323L15.2 7.92117V22.5221L9.6 25.6333V11.0323Z"
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M277.28 445.439C373.334 432.144 447.318 349.731 447.318 249.999V0H500V249.999C500 388.083 388.085 499.999 250.001 499.999L277.28 445.439Z"
+                fill="url(#paint0_linear_4167_15587)"
             />
+            <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M249.999 499.999C111.915 499.999 0 388.083 0 249.999H52.6819C52.6819 349.731 126.666 432.145 222.72 445.44L249.999 499.999Z"
+                fill="url(#paint1_linear_4167_15587)"
+            />
+            <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M382.07 0V258.16C382.07 330.765 322.644 390.229 250.001 390.229C177.357 390.229 117.932 330.804 117.932 258.16V250.038H170.614V258.16C170.614 301.723 206.438 337.547 250.001 337.547C293.564 337.547 329.388 301.723 329.388 258.16V0H382.07Z"
+                fill="url(#paint2_linear_4167_15587)"
+            />
+            <defs>
+                <linearGradient id="paint0_linear_4167_15587" x1="338.928" y1="359.769" x2="494.291" y2="237.126" gradientUnits="userSpaceOnUse">
+                    <stop stop-color="#3F83F8" />
+                    <stop offset="1" stop-color="#84E1BC" />
+                </linearGradient>
+                <linearGradient id="paint1_linear_4167_15587" x1="125" y1="249.999" x2="125" y2="499.999" gradientUnits="userSpaceOnUse">
+                    <stop stop-color="#16BDCA" />
+                    <stop offset="1" stop-color="#6875F5" />
+                </linearGradient>
+                <linearGradient id="paint2_linear_4167_15587" x1="382.07" y1="73.678" x2="144.292" y2="73.678" gradientUnits="userSpaceOnUse">
+                    <stop stop-color="#6875F5" />
+                    <stop offset="1" stop-color="#16BDCA" />
+                </linearGradient>
+            </defs>
         </svg>
     );
 }
