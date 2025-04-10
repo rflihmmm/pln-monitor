3bc7036fc8c3ba47b48dcb976e1214f50730017d
rflihmmm <indomilk04@gmail.com>
2025-04-10 19:05:37 +0800

    add manage-user page

---
+++ resources/js/pages/settings/manage-users.tsx
@@ -0,0 +1,28 @@
+import { Head } from '@inertiajs/react';
+
+import HeadingSmall from '@/components/heading-small';
+import { type BreadcrumbItem } from '@/types';
+
+import AppLayout from '@/layouts/app-layout';
+import SettingsLayout from '@/layouts/settings/layout';
+
+const breadcrumbs: BreadcrumbItem[] = [
+    {
+        title: 'Manage Users',
+        href: '/settings/manage-users',
+    },
+];
+
+export default function Appearance() {
+    return (
+        <AppLayout breadcrumbs={breadcrumbs}>
+            <Head title="Manage Users" />
+
+            <SettingsLayout>
+                <div className="space-y-6">
+                    <HeadingSmall title="Manage Users" description="Add or Delete user" />
+                </div>
+            </SettingsLayout>
+        </AppLayout>
+    );
+}
