d104f9aa123af61de27ec7d42ebc880f6014012f
rflihmmm <indomilk04@gmail.com>
2025-04-22 09:59:08 +0800

    change default theme from 'system' to 'light'

--- resources/js/hooks/use-appearance.tsx
+++ resources/js/hooks/use-appearance.tsx
@@ -64,7 +64,7 @@ export function useAppearance() {
 
     useEffect(() => {
         const savedAppearance = localStorage.getItem('appearance') as Appearance | null;
-        updateAppearance(savedAppearance || 'system');
+        updateAppearance(savedAppearance || 'light');
 
         return () => mediaQuery()?.removeEventListener('change', handleSystemThemeChange);
     }, [updateAppearance]);
