4155434d9b31f8b3283b6db92d4448b1ab37d702
firdauspallaka <firdaus@pallakastudio.com>
2025-04-23 17:33:48 +0800

    add Feeder and GarduInduk models with migrations for feeders and related tables

---
+++ app/Models/GarduInduk.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class GarduInduk extends Model
+{
+    protected $fillable = [
+        'name',
+        'description',
+    ];
+}
