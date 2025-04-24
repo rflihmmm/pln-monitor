d4aff8f7f8cde84f72bfac1ed6aff8da7716c30b
rflihmmm <indomilk04@gmail.com>
2025-04-24 13:29:01 +0800

    change href to optional type

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -18,7 +18,7 @@ export interface NavGroup {
 
 export interface NavItem {
     title: string;
-    href: string;
+    href?: string;
     icon?: LucideIcon | null;
     isActive?: boolean;
     canAccess?: (roles: string[]) => boolean;
