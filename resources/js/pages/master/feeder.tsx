6275d1ec978279645f6f8484e0501a1f35fa193a
rflihmmm <indomilk04@gmail.com>
2025-04-24 15:01:20 +0800

    update some stuff to connect to database

--- resources/js/pages/master/feeder.tsx
+++ resources/js/pages/master/feeder.tsx
@@ -2,21 +2,42 @@ import { Head } from '@inertiajs/react';
 
 import { type BreadcrumbItem } from '@/types';
 
+import TableFeeder from '@/components/table-feeder';
 import AppLayout from '@/layouts/app-layout';
 
+// Define interfaces for the data
+interface GarduInduk {
+  id: number;
+  name: string;
+}
+
+interface Feeder {
+  id: number;
+  name: string;
+  description: string | null;
+  gardu_induk_id: number;
+  created_at: string;
+  gardu_induk?: GarduInduk;
+}
+
+interface FeederPageProps {
+  feederList: Feeder[];
+  garduIndukList: GarduInduk[];
+}
+
 const breadcrumbs: BreadcrumbItem[] = [
-    {
-        title: 'Feeder',
-        href: 'master/feeder',
-    },
+  {
+    title: 'Feeder',
+    href: 'master/feeder',
+  },
 ];
 
-export default function GarduInduk() {
-    return (
-        <AppLayout breadcrumbs={breadcrumbs}>
-            <Head title="Feeder" />
+export default function FeederPage({ feederList, garduIndukList }: FeederPageProps) {
+  return (
+    <AppLayout breadcrumbs={breadcrumbs}>
+      <Head title="Feeder" />
 
-            <TableGarduInduk />
-        </AppLayout>
-    );
+      <TableFeeder feederList={feederList} garduIndukList={garduIndukList} />
+    </AppLayout>
+  );
 }
--- resources/js/pages/master/gardu-induk.tsx
+++ resources/js/pages/master/gardu-induk.tsx
@@ -5,19 +5,31 @@ import { type BreadcrumbItem } from '@/types';
 import TableGarduInduk from '@/components/table-gardu-induk';
 import AppLayout from '@/layouts/app-layout';
 
+// Define interfaces for the data
+interface GarduInduk {
+  id: number;
+  name: string;
+  description: string | null;
+  created_at: string;
+}
+
+interface GarduIndukPageProps {
+  garduIndukList: GarduInduk[];
+}
+
 const breadcrumbs: BreadcrumbItem[] = [
-    {
-        title: 'Gardu Induk',
-        href: 'master/gardu-induk',
-    },
+  {
+    title: 'Gardu Induk',
+    href: 'master/gardu-induk',
+  },
 ];
 
-export default function GarduInduk() {
-    return (
-        <AppLayout breadcrumbs={breadcrumbs}>
-            <Head title="Gardu Induk" />
+export default function GarduIndukPage({ garduIndukList }: GarduIndukPageProps) {
+  return (
+    <AppLayout breadcrumbs={breadcrumbs}>
+      <Head title="Gardu Induk" />
 
-            <TableGarduInduk />
-        </AppLayout>
-    );
+      <TableGarduInduk garduIndukList={garduIndukList} />
+    </AppLayout>
+  );
 }
