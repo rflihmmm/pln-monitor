bbca0e9a3acaac187d5d9f76d8bd51c883b4a395
rflihmmm <indomilk04@gmail.com>
2025-05-11 14:28:55 +0800

    add route to /test-mssql to test mssql databse connection

--- routes/web.php
+++ routes/web.php
@@ -11,20 +11,28 @@ use Inertia\Inertia;
 Route::redirect('/', '/login')->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
-     Route::get('dashboard', function () {
+    Route::get('dashboard', function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
-   Route::get('alarm', function () {
+    Route::get('alarm', function () {
         return Inertia::render('alarm');
     })->name('alarm');
-     Route::get('table-monitor', function () {
+    Route::get('table-monitor', function () {
         return Inertia::render('table-monitor');
     })->name('table-monitor');
-   Route::get('single-line', function () {
+    Route::get('single-line', function () {
         return Inertia::render('single-line');
     })->name('single-line');
+    Route::get('/test-mssql', function () {
+        try {
+            DB::connection('sqlsrv')->getPdo();
+            return 'Connected to MSSQL!';
+        } catch (\Exception $e) {
+            return 'Failed: ' . $e->getMessage();
+        }
+    });
 });
 
-require __DIR__.'/settings.php';
-require __DIR__.'/auth.php';
-require __DIR__.'/master.php';
+require __DIR__ . '/settings.php';
+require __DIR__ . '/auth.php';
+require __DIR__ . '/master.php';
