264936c1bacabb9fbf34294093d1b8a80af86998
rflihmmm <raflihamdani75@protonmail.com>
2025-09-11 15:29:06 +0800

    remove logo form footer

--- resources/js/layouts/app/app-footer.tsx
+++ resources/js/layouts/app/app-footer.tsx
@@ -6,7 +6,6 @@ export default function AppFooter({ className, ...props }: HTMLAttributes<HTMLEl
     return (
         <footer className={`bg-background border-t md:py-0 w-full ${className}`} {...props}>
             <div className="flex flex-row items-center justify-center py-2 px-2">
-                <AppLogo className="w-42" />
                 <div className='flex flex-col'>
                     <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
                         © 2025 <Link href="/" className="font-medium underline underline-offset-4">PLN Monitoring Dashboard</Link>. Support by Jubir.ai
